bf661138aa8242b18ea7ca5a74b17a41ba8a3644
markiMiracle <someonewhosome@list.ru>
2024-06-05 19:28:04 +0300

    add Javalin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,21 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
     application
     jacoco
     checkstyle
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.freefair.lombok") version "8.4"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
 
 repositories {
     mavenCentral()
@@ -17,6 +25,10 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.30")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
 }
 
 tasks.test {
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+    public static Javalin getApp() {
+
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+
+
+        app.get("/", ctx -> {
+            ctx.result("Hello, World!");
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        var app = getApp();
+        app.start(getPort());
+    }
+
+}
