c7fb3ca35182c2694660af1ce435aeeb39d2a3db
markiMiracle <someonewhosome@list.ru>
2024-06-10 00:28:38 +0300

    add JDBC_DATABASE_URL

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -30,7 +30,7 @@ public class App {
     }
     public static Javalin getApp() throws SQLException {
         var hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(System.getenv().getOrDefault("JAVA_DATABASE_URL",
+        hikariConfig.setJdbcUrl(System.getenv().getOrDefault("JDBC_DATABASE_UR",
                 "jdbc:h2:mem:project:DB_CLOSE_DELAY=-1;"));
         var dataSource = new HikariDataSource(hikariConfig);
         var url = App.class.getClassLoader().getResourceAsStream("schema.sql");
